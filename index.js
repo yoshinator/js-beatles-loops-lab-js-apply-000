d437a5b7c810b1d66fe0512c4d957b1cda637f89
yoan ante <yoanante@gmail.com>
2018-08-12 01:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   var arry = [];
   var i;
   for (i = 0; i < musicians.length; i++){
-    arry[i] = musicians[i] + instruments[i]
+    arry[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return arry;
 }
