d85ab322230930d47f9ae3ffe7fa518750ae6235
yoan ante <yoanante@gmail.com>
2018-08-12 01:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   var i = 0;
-  while(i <= facts.length ){
+  while(i < facts.length ){
     facts[i] = facts[i]+"!!!"
     i++;
   }
